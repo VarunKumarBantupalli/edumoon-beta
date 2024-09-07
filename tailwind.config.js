d69c5b6a987bd530d1daf5122dc160b28e3bd6e7
SriKavyaMedida <165463089+SriKavyaMedida@users.noreply.github.com>
2024-09-07 12:05:11 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["*.{html,js}"],
   theme: {
-    extend: {
-      keyframes: {
-        scroll: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-        scrollReverse: { 
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(100%)' },
-        },
-      },
-      animation: {
-        scroll: 'scroll 15s linear infinite',
-        scrollReverse: 'scrollReverse 15s linear infinite', 
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
