e3a7897295b10afa3e3fbe65ea8acc88f50a9b3b
VarunKumarBantupalli <bantupallivarun@gmail.com>
2024-09-11 23:32:56 +0530

    setting routes in the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 
-import Articles from "./utils/Articles"
-import SocialMedia from "./utils/SocialMedia"
-import Testimonial from "./utils/Testimonial"
-import EventImages from "./utils/EventImages"
 
 function App() {
   
 
   return (
     <>
-     <EventImages/>
-     <SocialMedia/>
-     <Articles/>
-     <Testimonial/>
+
 
 
     </>
