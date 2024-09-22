2d40ec3ae22f3ed9a917a4759f00f64bf903d32b
SriKavyaMedida <srikavyamedidha23@gmail.com>
2024-09-22 15:49:20 +0530

    about us page

--- src/App.jsx
+++ src/App.jsx
@@ -8,56 +8,41 @@ import Clubs from './components/Clubs';
 import Profile from './components/Profile';
 import Tutition from './components/Tutition';
 
-<<<<<<< HEAD
-=======
+// Importing additional components
 import Articles from "./utils/Articles"
 import SocialMedia from "./utils/SocialMedia"
 import Testimonial from "./utils/Testimonial"
 import EventImages from "./utils/EventImages"
 import ContactForm from "./utils/ContactForm"
 import Hiring from "./utils/Hiring"
->>>>>>> e4a89321b9d7f8674f22575cd43546de32abf142
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Course" element={<Course />} />
-        <Route path="/Aboutus" element={<Aboutus />} />
-        <Route path="/Events" element={<Events />} />
-        <Route path="/Clubs" element={<Clubs />} />
-        <Route path="/Tutition" element={<Tutition />} />
-        <Route path="/Profile" element={<Profile />} />
-      </Routes>
-    </Router>
+    <>
+      {/* Router Section */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Course" element={<Course />} />
+          <Route path="/Aboutus" element={<Aboutus />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/Clubs" element={<Clubs />} />
+          <Route path="/Tutition" element={<Tutition />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Routes>
+      </Router>
+
+      {/* Additional Components Section */}
+      <EventImages />
+      <SocialMedia />
+      <Articles />
+      <Testimonial />
+      <div className="flex justify-center items-center w-[100%] h-[100vh]">
+        <ContactForm />
+      </div>
+      <Hiring />
+    </>
   );
 }
 
 export default App;
-=======
-    <>
-     <EventImages/>
-     <SocialMedia/>
-     <Articles/>
-     <Testimonial/>
-     <div className=" flex justify-center items-center w-[100%] h-[100vh]">
-      <ContactForm/>
-     </div>
-     <Hiring/>
-
-
-    </>
-  )
-}
-
-export default App
-
-const ExtendedFamilySection = () => {
-  console.log("ExtendedFamilySection rendered");
-  return (
-    <section>...</section>
-  );
-};
->>>>>>> e4a89321b9d7f8674f22575cd43546de32abf142
--- src/components/Founders.jsx
+++ src/components/Founders.jsx
@@ -4,7 +4,6 @@ function Founders() {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto flex flex-wrap flex-col">
-        {/* Vikas Kapuganti */}
         <img
           className="xl:w-1/6 lg:w-1/4 md:w-1/3 w-1/2 block mx-auto mb-10 object-cover object-center rounded"
           alt="Vikas Kapuganti"
@@ -12,13 +11,12 @@ function Founders() {
         />
         <div className="flex flex-col text-center w-full">
           <h1 className="text-xl font-medium title-font mb-4 text-gray-900">
-            Vikas Kapuganti – Co-Founder & CEO, EduMoon
+            Vikas Kapuganti - Co-Founder & CEO, EduMoon
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Vikas Kapuganti is the co-founder and CEO of EduMoon, an ed-tech startup based in Visakhapatnam. Launched in 2020, EduMoon has grown into a community of over 12,000 students, offering courses, internships, and webinars to bridge the gap between academia and industry. Under Vikas' leadership, the startup has been valued at ₹3 crore and gained recognition in global competitions like the TiE University Global Pitch.
           </p>
         </div>
-        {/* Ponnada Mahidhar */}
         <img
           className="xl:w-1/6 lg:w-1/4 md:w-1/3 w-1/2 block mx-auto mb-10 object-cover object-center rounded"
           alt="Ponnada Mahidhar"
@@ -26,7 +24,7 @@ function Founders() {
         />
         <div className="flex flex-col text-center w-full">
           <h1 className="text-xl font-medium title-font mb-4 text-gray-900">
-            Ponnada Mahidhar – Co-Founder & CMO, EduMoon
+            Ponnada Mahidhar - Co-Founder & CMO, EduMoon
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Ponnada Mahidhar is the co-founder of EduMoon, an ed-tech startup that supports students with academic and career resources. Under his leadership, EduMoon has grown to over 12,000 members, offering courses and internships, and is valued at ₹3 crore. Mahidhar is focused on bridging the gap between education and industry while expanding the platform’s reach.
