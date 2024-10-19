33b460ae2f01f7d19ac336a42d49d3c7db703356
VarunKumarBantupalli <bantupallivarun@gmail.com>
2024-10-19 16:14:19 +0530

    deleting cluttered components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import logo from '../assets/images/socialmedialogos/edumoonlogo.png';
+
 import Testimonial from '../utils/Testimonial';
 import Stats from '../utils/Stats';
-import SocialMedia from '../utils/SocialMedia';
-import Lectures from './Lectures';
 import Webinar from './Webinar';
 import Workshops from './Workshops';
-import Hiring from './Hiring';
-import Outdooractivites from './Outdooractivites';
-import Mobileapp from './Mobileapp';
 import Articles from '../utils/Articles'
-
-
 import Trustedby from '../utils/Trustedby';
-import Clients from './Clients';
 import LandingPage from './LandingPage';
-
-import ClubsDisplay from './ClubsDisplay';
-
 import smthg from '../assets/images/other_images/hiring.jpg';
+import PopularCourses from './PopularCourses';
 
 import "swiper/css";
-import PopularCourses from './PopularCourses';
 
 function Home() {
   return (
--- src/components/Tutition.jsx
+++ src/components/Tutition.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Hiring from './Hiring'
-import Bookdemoclass from './Bookdemoclass'
+
 import Mentors from './Mentors'
-import Stats from '../utils/Stats'
+
 import Testimonial from '../utils/Testimonial'
 import Exploreus from './Exploreus'
 import PopularCourses from './PopularCourses'
@@ -13,8 +12,7 @@ import RegistrationButtons from './RegistrationButtons'
 function Tutition() {
   return (
     <>
-     {/* <Hiring/> */}
-     {/* <Bookdemoclass/> */}
+
      
      <Exploreus/>
      <RegistrationButtons/>
